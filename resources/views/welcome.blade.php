bad56e27eabc2b0cdfe013681a27c5e8dbca7046
foxxie_911 <foxxie911.dev@proton.me>
2024-12-06 11:16:20 +0600

    searchbus route changed to home route

--- resources/views/welcome.blade.php
+++
@@ -1,3 +0,0 @@
-<x-layout>
-    <h1>Something</h1>
-</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,7 @@ use App\Http\Controllers\BusController;
 use App\Http\Controllers\CompanyController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-// Route::get('/findcompany', function () {
-//     $options = Location::all();
-//     return view('findcompany', ['options' => $options]);
-// });
-
-Route::view('/searchbus', 'companies', ['companies' => array()]);
-Route::post('/searchbus', [CompanyController::class, 'find']);
+Route::view('/', 'companies', ['companies' => array()]);
+Route::post('/', [CompanyController::class, 'find']);
 
 Route::get('/buses/{company}', [BusController::class, 'stopages']);
